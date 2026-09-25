1e290f27259d6bc99688e90d302e348532431bfd
Suhani patel <suhanipatel773@gmail.com>
2026-09-24 21:28:07 -0700

    All Arithematic Operations together

---
+++ LAB-2/All Arithematic Operations.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main ()
+{
+    int a,b,c;
+    a=50;
+    b=10;
+    c=a+b;
+    printf("%d",c);
+    c=a-b;
+    printf("\n%d",c);
+    c=a*b;
+    printf("\n%d",c);
+    c=a/b;
+    printf("\n%d",c);
+    c=a%b;
+    printf("\n%d",c);
+    return 0;
+}
